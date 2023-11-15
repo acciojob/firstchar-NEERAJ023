028bfdca9d38aefe105b1b4fc58c88e64059d232
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-11-15 22:40:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstChar(text) {
   // your code here
+	for(let k=0; k<text.length; k++){
+		if(text[k]!==" "){
+			return text[k];
+		}
+	}
+	return "";
 }
 
 // Do not change the code below
 
 const text = prompt("Enter text:");
 alert(firstChar(text));
+
+
